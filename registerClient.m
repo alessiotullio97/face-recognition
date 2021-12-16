95c726916116aa7791e6559c726b3e0cfa661ed5
Elisabetta Chiusoli <47059153+elisabettachiusoli@users.noreply.github.com>
2021-12-16 15:42:22 +0100

    Regiter Yourselves button implemented with previous scripts
    Register Yourselves button start registerClient script on matlab; Start button start takeSnapshots and saveSnapshots. All scripts works correctly.

--- registerClient.m
+++ registerClient.m
@@ -1,30 +1,13 @@
 function [result] = registerClient(app)
         % Enter your name and register your face within the database
-        app.EnterYourNameEditField.Visible = true;
-        tlabel = uilabel(app.EnterYourNameEditField);
-        tlabel.Text = "What's your name?";
+        app.RegisterYourselvesPanel.Visible = true;
+        answer = app.EnterYourNameEditField.Value;
+        app.IdentificationModeButton.Enable=false;
+        app.VerificationModeButton.Enable=false;
+        app.RegisterYourselvesButton.Enable=false;
+         num = 10;
+        app.Label.Text= sprintf('%s\n%s',"Welcome " + answer+ "!","Now I will take "+ string(num)+ " snapshots.");
+ return;   
         
-        return;
-        prompt = "What's your name?";
         
-        % dialog box ask your name and get your answer
-        answer = inputdlg(prompt);
-        
-        % numeber of screeshots to be performed
-        num = 10;
-        
-        % dialog box showing your name taken
-        f = msgbox(['Welcome ' answer{1} '! Now I will take ' string(num) ...
-                ' snapshots.']);
-        
-        defaultDB = 'database';
-        currentDir = pwd();
-        dbPath = fullfile(currentDir, defaultDB);
-        
-        try
-                result = takeSnapshots(answer{1}, dbPath, num);
-        catch
-                warning("Unable to take 10 snapshots.");
-                result = -1;
-        end
-end
+      
